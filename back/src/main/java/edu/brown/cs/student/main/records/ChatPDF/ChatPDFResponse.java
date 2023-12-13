f6d8c3032b3829df087da429f8d124ada8406fc4
sbwest051 <sbwest051@gmail.com>
2023-12-12 22:46:18 -0500

    partner coding solving merge conflicts

--- back/src/main/java/edu/brown/cs/student/main/records/ChatPDF/ChatPDFResponse.java
+++
@@ -1,7 +0,0 @@
-package edu.brown.cs.student.main.records.ChatPDF;
-
-import java.util.List;
-
-public record ChatPDFResponse(String sourceId, String content, List<reference> references) {
-
-}
--- back/src/main/java/edu/brown/cs/student/main/records/maps/Feature.java
+++
@@ -1,8 +0,0 @@
-package edu.brown.cs.student.main.records.maps;
-
-public record Feature(String type, geometry geometry, properties properties) {
-
-  public Feature(geometry geometry, properties properties) {
-    this("Feature", geometry, properties);
-  }
-}
