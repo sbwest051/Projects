650976c1f609f9298bcf116da3d5aaf81fcb2b42
sbwest051 <sbwest051@gmail.com>
2023-12-17 21:26:07 -0500

    fixed table position

--- frontend/src/components/frontendJSON.tsx
+++ frontend/src/components/frontendJSON.tsx
@@ -16,10 +16,39 @@ export type Source = {
 export type Column = {
   title: string;
   question: string;
-} & ({ keywordList: any } | { keywordMap: any }); // TODO: need to figure out how to make this more specific
+} &({ keywordList: any } | { keywordMap: { [key: string]: any } });
 
 // Define the SourceData type as a tuple that represents the structure of data inputs.
 export type SourceData = [string, string, string]; // Tuple of [pdfType, title, link/filepath]
+
+function keyMapHelper(pairs: string[]){
+  const keyValuePairs = pairs.map(piece => piece.trim());
+
+// Initialize an empty object
+    const keywordMap: { [key: string]: any } = {};
+
+    // Iterate over each key-value pair string
+    keyValuePairs.forEach(pair => {
+      // Check if the pair contains a colon, indicating a key-value structure
+      if (pair.includes(':')) {
+        // Split by colon to separate the key from the value
+        const [key, value] = pair.split(':').map(part => part.trim());
+        // If the key doesn't exist in the object, initialize it with an empty array
+        if (!keywordMap[key]) {
+          keywordMap[key] = [];
+        }
+        // Push the value into the corresponding array, splitting by commas if necessary
+        keywordMap[key].push(...value.split(',').map(v => v.trim()));
+      } else {
+        // If there is no colon, assume it is a single value and add it to a 'default' key
+        if (!keywordMap['default']) {
+          keywordMap['default'] = [];
+        }
+        keywordMap['default'].push(pair);
+      }
+    });
+    }
+
 
 // This function takes an array of SourceData and maps it to an array of Source.
 export function constructJSON(dataValues: SourceData[], queryTitle: string, question: string, keywords: string): JSONStructure {
